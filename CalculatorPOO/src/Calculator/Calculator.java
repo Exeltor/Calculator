e61ff230b58da47161d2ace8d29474e9b682032f
Exeltor <zhelezovalexey@gmail.com>
2018-03-30 15:52:11 +0200

    Aumentado tamaño de la letra un poco

--- CalculatorPOO/src/Calculator/Calculator.java
+++ CalculatorPOO/src/Calculator/Calculator.java
@@ -11,6 +11,7 @@ import Operations.*;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
 
 
 public class Calculator {
@@ -53,11 +54,13 @@ public class Calculator {
 		frame.getContentPane().setLayout(null);
 		
 		JTextPane Screen = new JTextPane();
+		Screen.setFont(new Font("Tahoma", Font.PLAIN, 23));
 		Screen.setEditable(false);
 		Screen.setBounds(21, 21, 382, 46);
 		frame.getContentPane().add(Screen);
 		
 		JButton btn7 = new JButton("7");
+		btn7.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btn7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				input += "7";
@@ -68,6 +71,7 @@ public class Calculator {
 		frame.getContentPane().add(btn7);
 		
 		JButton btn8 = new JButton("8");
+		btn8.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btn8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				input += "8";
@@ -78,6 +82,7 @@ public class Calculator {
 		frame.getContentPane().add(btn8);
 		
 		JButton btn9 = new JButton("9");
+		btn9.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btn9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				input += "9";
@@ -88,6 +93,7 @@ public class Calculator {
 		frame.getContentPane().add(btn9);
 		
 		JButton btnSub = new JButton("-");
+		btnSub.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btnSub.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				side1 = Screen.getText();
@@ -100,6 +106,7 @@ public class Calculator {
 		frame.getContentPane().add(btnSub);
 		
 		JButton btn4 = new JButton("4");
+		btn4.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btn4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				input += "4";
@@ -110,6 +117,7 @@ public class Calculator {
 		frame.getContentPane().add(btn4);
 		
 		JButton btn5 = new JButton("5");
+		btn5.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btn5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				input += "5";
@@ -120,6 +128,7 @@ public class Calculator {
 		frame.getContentPane().add(btn5);
 		
 		JButton btn6 = new JButton("6");
+		btn6.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btn6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				input += "6";
@@ -130,6 +139,7 @@ public class Calculator {
 		frame.getContentPane().add(btn6);
 		
 		JButton btn1 = new JButton("1");
+		btn1.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btn1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				input += "1";
@@ -140,6 +150,7 @@ public class Calculator {
 		frame.getContentPane().add(btn1);
 		
 		JButton btn2 = new JButton("2");
+		btn2.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btn2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				input += "2";
@@ -150,6 +161,7 @@ public class Calculator {
 		frame.getContentPane().add(btn2);
 		
 		JButton btn3 = new JButton("3");
+		btn3.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btn3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				input += "3";
@@ -160,6 +172,7 @@ public class Calculator {
 		frame.getContentPane().add(btn3);
 		
 		JButton btn0 = new JButton("0");
+		btn0.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btn0.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				input += "0";
@@ -170,6 +183,7 @@ public class Calculator {
 		frame.getContentPane().add(btn0);
 		
 		JButton btnDot = new JButton(".");
+		btnDot.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btnDot.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				input += ".";
@@ -180,6 +194,7 @@ public class Calculator {
 		frame.getContentPane().add(btnDot);
 		
 		JButton btnPlusMinus = new JButton("+-");
+		btnPlusMinus.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btnPlusMinus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -188,6 +203,7 @@ public class Calculator {
 		frame.getContentPane().add(btnPlusMinus);
 		
 		JButton btnMul = new JButton("*");
+		btnMul.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btnMul.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				side1 = Screen.getText();
@@ -200,6 +216,7 @@ public class Calculator {
 		frame.getContentPane().add(btnMul);
 		
 		JButton btnDiv = new JButton("/");
+		btnDiv.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btnDiv.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				side1 = Screen.getText();
@@ -212,6 +229,7 @@ public class Calculator {
 		frame.getContentPane().add(btnDiv);
 		
 		JButton btnEq = new JButton("=");
+		btnEq.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btnEq.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String result;
@@ -249,6 +267,7 @@ public class Calculator {
 		frame.getContentPane().add(btnEq);
 		
 		JButton btnBk = new JButton("BK");
+		btnBk.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btnBk.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (Screen.getText().length() > 0) {
@@ -263,6 +282,7 @@ public class Calculator {
 		frame.getContentPane().add(btnBk);
 		
 		JButton btnRes = new JButton("C");
+		btnRes.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btnRes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				input = "";
@@ -273,6 +293,7 @@ public class Calculator {
 		frame.getContentPane().add(btnRes);
 		
 		JButton btnPow = new JButton("^");
+		btnPow.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btnPow.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				side1 = Screen.getText();
@@ -285,6 +306,7 @@ public class Calculator {
 		frame.getContentPane().add(btnPow);
 		
 		JButton btnSum = new JButton("+");
+		btnSum.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		btnSum.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				side1 = Screen.getText();
