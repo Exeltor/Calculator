92918f670f5c3627f5b630a36cb99d28cac69510
AlmohadaSexy <itrigueropedraza@gmail.com>
2018-03-30 21:16:49 +0200

    Log

--- CalculatorPOO/src/Operations/Log.java
+++ CalculatorPOO/src/Operations/Log.java
@@ -1,5 +1,13 @@
 package Operations;
 
-public class Log {
-
+public class Log extends Operation{
+	
+	public Log( double operand1, double operand2) {
+		super(operand1, operand2);
+	}
+	
+	public String operate() {
+		double operation = Math.log(super.operand1) / Math.log(super.operand2);
+		return Double.toString(operation);
+	}
 }
